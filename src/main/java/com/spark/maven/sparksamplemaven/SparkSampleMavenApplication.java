bac57dee391edf997a5e2cf06baa3f8fc7e07163
jun-roh <jun.roh85@gmail.com>
2021-06-24 21:45:44 +0900

    - change settings - database configuration

--- src/main/java/com/spark/maven/sparksamplemaven/SparkSampleMavenApplication.java
+++ src/main/java/com/spark/maven/sparksamplemaven/SparkSampleMavenApplication.java
@@ -2,10 +2,10 @@ package com.spark.maven.sparksamplemaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SparkSampleMavenApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SparkSampleMavenApplication.class, args);
     }
